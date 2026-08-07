db5d3306f4a049cc9ac73b1d639f65ee2305a8cd
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-07 23:32:59 +0800

    style: format string initializer signature

--- src/frontend/parser_string.c
+++ src/frontend/parser_string.c
@@ -82,10 +82,7 @@ static bool decoded_string_length(MinicParser *parser, MinicSourceSpan span, siz
 }
 
 static bool
-add_string_initializers(
-    MinicParser *parser,
-    MinicSourceSpan span,
-    MinicGlobalObjectId object_id) {
+add_string_initializers(MinicParser *parser, MinicSourceSpan span, MinicGlobalObjectId object_id) {
     size_t cursor;
     size_t end;
 
