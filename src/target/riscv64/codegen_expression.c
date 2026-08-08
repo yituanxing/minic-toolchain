12314291682b15622a3116d5c1e0aa812dbf0038
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-08 13:30:29 +0800

    style: simplify sizeof layout emission

--- src/target/riscv64/codegen_expression.c
+++ src/target/riscv64/codegen_expression.c
@@ -353,12 +353,13 @@ bool minic_riscv64_emit_expression(FILE *file,
     case MINIC_EXPRESSION_GLOBAL_OBJECT:
         return false;
     case MINIC_EXPRESSION_SIZEOF: {
+        MinicType measured_type;
         size_t alignment;
         size_t size;
 
+        measured_type = expression->value.sizeof_type;
         if (!minic_type_equal(expression->type, minic_type_unsigned_long()) ||
-            !minic_riscv64_type_layout(
-                program, expression->value.sizeof_type, &size, &alignment)) {
+            !minic_riscv64_type_layout(program, measured_type, &size, &alignment)) {
             return false;
         }
         return fprintf(file, "  li a0, %zu\n", size) >= 0;
