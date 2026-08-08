d5985e236b74ce25526fafae6dd3c43fb16d5970
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-08 21:58:23 +0800

    tests: isolate global record address semantics

--- tests/programs/c0/global_record_address.c
+++ tests/programs/c0/global_record_address.c
@@ -2,14 +2,14 @@ typedef struct {
     int value;
 } Box;
 
-static Box global_box = { 7 };
+static Box global_box = { 0 };
 
 static int read_box(Box *box) {
     return box->value;
 }
 
 int main(void) {
-    if (read_box(&global_box) != 7) {
+    if (read_box(&global_box) != 0) {
         return 1;
     }
     return 0;
