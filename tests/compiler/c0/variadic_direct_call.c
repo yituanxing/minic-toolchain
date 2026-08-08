949b34e3d79dc872eb8f787e85d5f6f2a71748f1
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-08 22:07:58 +0800

    tests: cover double variadic arguments

--- tests/compiler/c0/variadic_direct_call.c
+++ tests/compiler/c0/variadic_direct_call.c
@@ -5,9 +5,11 @@ int main(void)
     char small;
     long wide;
     int value;
+    double precise;
 
     small = 7;
     wide = 1234;
     value = 29;
-    return verify_variadic(5, 11, small, wide, &value);
+    precise = 2.5;
+    return verify_variadic(5, 11, small, wide, &value, precise);
 }
