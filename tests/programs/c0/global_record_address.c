ae839a2f28d03606e53cc7cbea5765e15e2e2e63
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-08 21:55:31 +0800

    tests: add global record address differential

---
+++ tests/programs/c0/global_record_address.c
@@ -0,0 +1,16 @@
+typedef struct {
+    int value;
+} Box;
+
+static Box global_box = { 7 };
+
+static int read_box(Box *box) {
+    return box->value;
+}
+
+int main(void) {
+    if (read_box(&global_box) != 7) {
+        return 1;
+    }
+    return 0;
+}
