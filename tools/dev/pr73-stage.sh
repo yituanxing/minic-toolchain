198ece8fba07601b1a6d29cb9ec936aeb8d9db93
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-09 15:04:51 +0800

    ci: enable short integer semantics / CI：启用 short 整数语义

--- tools/dev/pr73-stage.sh
+++ tools/dev/pr73-stage.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 set -eu
 
+python3 tools/dev/pr73-short-integer.py
 python3 tools/dev/pr73-function-pointer-parameter.py
 python3 tools/dev/pr73-extern-object.py
 python3 tools/dev/pr73-function-type-typedef.py
