891d77399a356fb23fd75ce78c9870ae52bff225
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-09 19:32:54 +0800

    test(lua): keep length-one array probe focused

--- tests/compiler/c0/record_length_one_array.c
+++ tests/compiler/c0/record_length_one_array.c
@@ -1,13 +1,8 @@
 struct Holder {
     int values[1];
+    int scalar;
 };
 
 int read_first(struct Holder *holder) {
-    return holder->values[0];
-}
-
-int main(void) {
-    struct Holder holder;
-    holder.values[0] = 7;
-    return sizeof(holder) == 4 && read_first(&holder) == 7 ? 0 : 1;
+    return holder->values[0] + holder->scalar;
 }
