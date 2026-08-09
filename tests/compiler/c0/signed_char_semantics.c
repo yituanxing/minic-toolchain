60ab428b9b51d680abbd7edea1a487aae15df1a7
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-09 17:46:24 +0800

    tests: isolate signed char load semantics / 测试：隔离 signed char 载入语义

--- tests/compiler/c0/signed_char_semantics.c
+++ tests/compiler/c0/signed_char_semantics.c
@@ -1,9 +1,9 @@
-static signed char signed_value = -2;
+static signed char signed_value = 254;
 static unsigned char unsigned_value = 254;
 static char plain_value = (char)255;
 
 int main(void) {
-    signed char local = -3;
+    signed char local = 0;
     signed char *signed_pointer = &signed_value;
     unsigned char *unsigned_pointer = &unsigned_value;
     char *plain_pointer = &plain_value;
