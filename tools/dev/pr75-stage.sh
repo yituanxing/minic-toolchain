0ea37642dfab8d9e10445f75e339a8d0bfcfd50c
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-09 20:09:57 +0800

    ci: stage enum constant expressions / CI：暂存枚举常量表达式

--- tools/dev/pr75-stage.sh
+++ tools/dev/pr75-stage.sh
@@ -14,6 +14,7 @@ python3 tools/dev/pr75-extern-function-declarations.py
 python3 tools/dev/pr75-anonymous-record-type-specifiers.py
 python3 tools/dev/pr75-union-local-declaration.py
 python3 tools/dev/pr75-enum-typedef.py
+python3 tools/dev/pr75-enum-constant-expressions.py
 python3 tools/dev/pr75-record-forward-declarations.py
 python3 tools/dev/pr75-record-top-level-dispatch.py
 python3 tools/dev/pr75-record-multi-declarators.py
