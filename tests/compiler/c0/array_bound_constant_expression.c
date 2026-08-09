cff7dcd4865e3cae0e230a0092775d18a2c4d39b
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-09 16:40:25 +0800

    test: exercise local constant-expression array bounds

--- tests/compiler/c0/array_bound_constant_expression.c
+++ tests/compiler/c0/array_bound_constant_expression.c
@@ -1,10 +1,18 @@
-static int multiplied[1024 * 1024];
-static unsigned char grouped[(2 + 3) * (8 - 1)];
-static int divided[(24 / 3) + (7 % 4)];
+int multiplied_size(void) {
+    char multiplied[1024 * 1024];
+    return (int)sizeof(multiplied);
+}
+
+int grouped_size(void) {
+    unsigned char grouped[(2 + 3) * (8 - 1)];
+    return (int)sizeof(grouped);
+}
+
+int divided_size(void) {
+    int divided[(24 / 3) + (7 % 4)];
+    return (int)sizeof(divided);
+}
 
 int main(void) {
-    return sizeof(multiplied) == 4194304 && sizeof(grouped) == 35 &&
-                   sizeof(divided) == 44
-               ? 0
-               : 1;
+    return multiplied_size() == 1048576 && grouped_size() == 35 && divided_size() == 44 ? 0 : 1;
 }
