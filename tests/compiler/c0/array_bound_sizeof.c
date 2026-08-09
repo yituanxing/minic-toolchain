c9444a5f664e794335783f07b5d6b965a5af6ddc
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-10 00:49:22 +0800

    tests: cover aggregate typedef sizeof bounds

--- tests/compiler/c0/array_bound_sizeof.c
+++ tests/compiler/c0/array_bound_sizeof.c
@@ -3,15 +3,20 @@ enum {
     SIZE_TOTAL
 };
 
+typedef struct SignalSet {
+    unsigned long words[16];
+} SignalSet;
+
 struct Sizes {
     unsigned char pointer_bytes[(sizeof(void *))];
     int arithmetic[sizeof(long) + 2 * sizeof(short)];
     int enum_bound[SIZE_TOTAL];
     int cast_bound[((int)(SIZE_TOTAL) + 1)];
     unsigned char literal_bytes[sizeof("\x1bLua") + sizeof("\x19\x93\r\n\x1a\n")];
+    unsigned char aggregate_bytes[256 - sizeof(SignalSet)];
 };
 
 int read_sizes(struct Sizes *sizes) {
     return sizes->pointer_bytes[7] + sizes->arithmetic[11] + sizes->enum_bound[5] +
-           sizes->cast_bound[6] + sizes->literal_bytes[11];
+           sizes->cast_bound[6] + sizes->literal_bytes[11] + sizes->aggregate_bytes[127];
 }
