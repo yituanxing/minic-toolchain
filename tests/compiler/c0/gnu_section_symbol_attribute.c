60a1a5e7e6c5bc02aa1c39699a59c957fa9e5290
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-11 01:36:14 +0800

    tests: cover deferred prefix section on extern object

--- tests/compiler/c0/gnu_section_symbol_attribute.c
+++ tests/compiler/c0/gnu_section_symbol_attribute.c
@@ -9,6 +9,14 @@ unsigned long suffix_second = 9;
 extern char suffix_array[] __attribute__((__section__(".probe.suffix.array")));
 char suffix_array[] = "z";
 
+struct prefix_section_shape {
+    unsigned long value;
+};
+
+extern __attribute__((section(".probe.prefix.concat" "")))
+__typeof__(struct prefix_section_shape) prefix_section_record;
+struct prefix_section_shape prefix_section_record = {11};
+
 void __attribute__((__section__(".probe.text"))) placed_function(void);
 
 void placed_function(void) {
@@ -17,7 +25,7 @@ void placed_function(void) {
 int main(void) {
     placed_function();
     return placed_data[0] == 'x' && suffix_first == 7 && suffix_second == 9 &&
-                   suffix_array[0] == 'z'
+                   suffix_array[0] == 'z' && prefix_section_record.value == 11
                ? 0
                : 1;
 }
