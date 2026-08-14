420a89658eea102e91f03ed0d7c922d3c456bb8e
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-14 23:39:23 +0800

    test: cover inferred static integer array bound

---
+++ tests/compiler/c0/static_inferred_integer_array.c
@@ -0,0 +1,11 @@
+typedef unsigned int u32;
+
+static const u32 runnable_avg_yN_inv[] __attribute__((__unused__)) = {
+    1,
+    2,
+    3,
+};
+
+int main(void) {
+    return sizeof(runnable_avg_yN_inv) == 3U * sizeof(u32) ? 0 : 1;
+}
