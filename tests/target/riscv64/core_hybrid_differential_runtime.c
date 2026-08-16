3abe0d349b670867e3c73685c1a7afe1a3f11d0f
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-17 00:48:52 +0800

    test(core): execute hybrid FIELD_ADDRESS island

--- tests/target/riscv64/core_hybrid_differential_runtime.c
+++ tests/target/riscv64/core_hybrid_differential_runtime.c
@@ -7,7 +7,7 @@ struct CoreHybridLayout {
 
 int core_hybrid_core(int value);
 int core_hybrid_call(int value);
-int core_hybrid_field(struct CoreHybridLayout *item);
+int *core_hybrid_field(struct CoreHybridLayout *item);
 int core_hybrid_fallback_load(int *value);
 
 int main(void) {
@@ -20,7 +20,7 @@ int main(void) {
     (void)printf("%d %d %d %d\n",
                  core_hybrid_core(5),
                  core_hybrid_call(10),
-                 core_hybrid_field(&layout),
+                 *core_hybrid_field(&layout),
                  core_hybrid_fallback_load(&value));
     return 0;
 }
