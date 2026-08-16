c3e39937e25af17398391b38ca7c8867da51a9ba
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-16 04:21:06 +0000

    frontend: compose inferred external record arrays [publish]

---
+++ tests/compiler/c0/external_inferred_record_array.c
@@ -0,0 +1,64 @@
+struct riscv_isa_ext_data {
+    const char *name;
+    const char *property;
+    unsigned int id;
+};
+
+extern const struct riscv_isa_ext_data riscv_isa_ext[];
+
+const struct riscv_isa_ext_data riscv_isa_ext[] = {
+    {
+        .name = "zicntr",
+        .property = "riscv,isa-base",
+        .id = 1,
+    },
+    {
+        .name = "zifencei",
+        .property = 0,
+        .id = 2,
+    },
+};
+
+struct stats_desc {
+    unsigned int flags;
+    int exponent;
+    unsigned int size;
+    unsigned int bucket_size;
+    unsigned int offset;
+};
+
+struct kvm_stats_desc {
+    struct stats_desc desc;
+    char name[16];
+};
+
+const struct kvm_stats_desc kvm_stats[] = {
+    {
+        {
+            .flags = 1,
+            .exponent = -9,
+            .size = 1,
+            .bucket_size = 0,
+            .offset = 4,
+        },
+        .name = "halt_wait",
+    },
+    {
+        {
+            .flags = 2,
+            .exponent = 0,
+            .size = 8,
+            .bucket_size = 1,
+            .offset = 12,
+        },
+        .name = "signal",
+    },
+};
+
+const struct riscv_isa_ext_data fixed_record_array[1] = {
+    {
+        .name = "fixed",
+        .property = 0,
+        .id = 3,
+    },
+};
