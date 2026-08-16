58e46040a3295103c2cc0fc3790c3e0b8404ca36
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-16 22:50:54 +0800

    test(core): exercise pointer ingress in differential

--- tests/target/riscv64/core_frontend_differential_runtime.c
+++ tests/target/riscv64/core_frontend_differential_runtime.c
@@ -12,7 +12,7 @@ int core_diff_ninth(int a0,
                     int a6,
                     int a7,
                     int a8);
-int core_diff_pointer(int *value);
+int core_diff_pointer_zero(int *value);
 
 int main(void) {
     static const int values[] = {-7, -1, 0, 1, 9};
@@ -34,6 +34,7 @@ int main(void) {
     }
     (void)printf("ninth %d\n", core_diff_ninth(1, 2, 3, 4, 5, 6, 7, 8, 91));
     pointed = -1234;
-    (void)printf("pointer %d\n", core_diff_pointer(&pointed));
+    (void)printf("pointer-nonnull %d\n", core_diff_pointer_zero(&pointed));
+    (void)printf("pointer-null %d\n", core_diff_pointer_zero((int *)0));
     return 0;
 }
