f98f85b410c4b1f1118afa8b786ae98137961b52
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-17 00:42:23 +0800

    test(core): execute hybrid field address routing

--- tests/target/riscv64/core_hybrid_differential_runtime.c
+++ tests/target/riscv64/core_hybrid_differential_runtime.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 
+struct CoreHybridLayout {
+    char prefix;
+    int value;
+};
+
 int core_hybrid_core(int value);
 int core_hybrid_call(int value);
+int core_hybrid_field(struct CoreHybridLayout *item);
 int core_hybrid_fallback_load(int *value);
 
 int main(void) {
+    struct CoreHybridLayout layout;
     int value;
 
+    layout.prefix = 7;
+    layout.value = 53;
     value = 37;
-    (void)printf("%d %d %d\n",
+    (void)printf("%d %d %d %d\n",
                  core_hybrid_core(5),
                  core_hybrid_call(10),
+                 core_hybrid_field(&layout),
                  core_hybrid_fallback_load(&value));
     return 0;
 }
