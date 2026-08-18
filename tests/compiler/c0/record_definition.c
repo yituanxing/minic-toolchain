b8eb9253cdfa0904a50c5e80d09c76df198a59b1
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-18 12:05:01 +0800

    frontend: parse pointer-to-array record fields (#322)

--- tests/compiler/c0/record_definition.c
+++ tests/compiler/c0/record_definition.c
@@ -8,6 +8,12 @@ typedef struct {
     unsigned long position;
 } error;
 
+struct pointer_array_fields {
+    char (*zero)[0];
+    char (*one)[1];
+    const unsigned char (*two)[2];
+};
+
 int main(void)
 {
     return 0;
