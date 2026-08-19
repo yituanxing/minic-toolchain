c10c939a9baaf6cc348007fde78fb2615eaaedc6
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-19 23:48:34 +0800

    ci: remove completed static record array designator formatter

--- tools/dev/format-static-record-array-member-designator-v1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-set -eu
-clang-format -i src/frontend/parser_function.c src/frontend/parser_global.c
-clang-format --dry-run --Werror src/frontend/parser_function.c src/frontend/parser_global.c
