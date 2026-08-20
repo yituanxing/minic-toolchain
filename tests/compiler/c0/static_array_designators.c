79b47b6a2361d9661592c261215025bfe0dc2e0b
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-20 19:17:19 +0800

    test: cover predefinition multidim array decay

--- tests/compiler/c0/static_array_designators.c
+++ tests/compiler/c0/static_array_designators.c
@@ -47,6 +47,11 @@ static const unsigned long chained_sizeof_bound[64 + 1]
 extern const unsigned long chained_redeclared[64 + 1]
                                                    [(((64) + ((sizeof(long) * 8)) - 1) /
                                                      ((sizeof(long) * 8)))];
+
+static const unsigned long *chained_redeclared_row(unsigned long cpu) {
+    return chained_redeclared[1 + cpu % 64];
+}
+
 const unsigned long chained_redeclared[64 + 1]
                                       [(((64) + ((sizeof(long) * 8)) - 1) /
                                         ((sizeof(long) * 8)))] = {
@@ -57,5 +62,6 @@ const unsigned long chained_redeclared[64 + 1]
 int main(void) {
     return (int)(indexed[1] + ranged[1] + (syscall_shape[0] != 0) + (names[2] != 0) +
                  mutable_inferred[3] + chained[1][0] + chained[2][1] +
-                 chained_sizeof_bound[1][0] + chained_redeclared[1][0]);
+                 chained_sizeof_bound[1][0] + chained_redeclared[1][0] +
+                 chained_redeclared_row(0)[0]);
 }
