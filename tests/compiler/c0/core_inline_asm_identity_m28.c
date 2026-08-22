9944f5a68cbd2e11dd25e5ccd6fefbb5d46bf89c
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-22 23:07:40 +0800

    test: stage Core inline asm identity M28

---
+++ tests/compiler/c0/core_inline_asm_identity_m28.c
@@ -0,0 +1,19 @@
+unsigned long core_m28_inline_asm_identity(unsigned long value) {
+    __asm__("" : "+rm"(value));
+    return value;
+}
+
+unsigned int core_m28_iter_div_u64_rem(unsigned long dividend,
+                                       unsigned int divisor,
+                                       unsigned long *remainder) {
+    unsigned int ret;
+
+    ret = 0;
+    while (dividend >= divisor) {
+        __asm__("" : "+rm"(dividend));
+        dividend -= divisor;
+        ret++;
+    }
+    *remainder = dividend;
+    return ret;
+}
