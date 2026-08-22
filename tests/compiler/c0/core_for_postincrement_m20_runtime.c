9e2dee0b0cd3d81774129bfed6f21ae4892a845d
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-22 18:49:18 +0800

    test: model Core M20 list head with sentinel

--- tests/compiler/c0/core_for_postincrement_m20_runtime.c
+++ tests/compiler/c0/core_for_postincrement_m20_runtime.c
@@ -10,17 +10,19 @@ unsigned long core_m20_list_count_nodes(struct core_m20_node *head);
 
 int main(void) {
     struct core_m20_node empty;
+    struct core_m20_node head;
     struct core_m20_node a;
     struct core_m20_node b;
     struct core_m20_node c;
 
     empty.next = &empty;
+    head.next = &a;
     a.next = &b;
     b.next = &c;
-    c.next = &a;
+    c.next = &head;
 
     printf("post=%lu,%lu\n", core_m20_postincrement(0), core_m20_postincrement(41));
-    printf("assign=%lu,%lu\n", core_m20_for_assign(&empty), core_m20_for_assign(&a));
-    printf("count=%lu,%lu\n", core_m20_list_count_nodes(&empty), core_m20_list_count_nodes(&a));
+    printf("assign=%lu,%lu\n", core_m20_for_assign(&empty), core_m20_for_assign(&head));
+    printf("count=%lu,%lu\n", core_m20_list_count_nodes(&empty), core_m20_list_count_nodes(&head));
     return 0;
 }
