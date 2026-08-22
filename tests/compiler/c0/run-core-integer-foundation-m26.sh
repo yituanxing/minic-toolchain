9108e4d40684e81b225f5ab4ca4ba86bcebeca43
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-22 22:03:40 +0800

    stage M26 focused runner

---
+++ tests/compiler/c0/run-core-integer-foundation-m26.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+set -eu
+: "${MINIC:?set MINIC}"
+: "${RISCV_CC:=riscv64-linux-gnu-gcc}"
+: "${QEMU_RISCV64:=qemu-riscv64}"
+: "${BUILD_DIR:=build/core-integer-foundation-m26}"
+mkdir -p "$BUILD_DIR"
+MINIC_CORE_IR=strict "$MINIC" -S tests/compiler/c0/core_integer_foundation_m26.c -o "$BUILD_DIR/minic.s"
+"$RISCV_CC" -O0 -static tests/compiler/c0/core_integer_foundation_m26_runtime.c "$BUILD_DIR/minic.s" -o "$BUILD_DIR/minic.elf"
+"$QEMU_RISCV64" "$BUILD_DIR/minic.elf"
+"$RISCV_CC" -O0 -static tests/compiler/c0/core_integer_foundation_m26_runtime.c tests/compiler/c0/core_integer_foundation_m26.c -o "$BUILD_DIR/gcc.elf"
+"$QEMU_RISCV64" "$BUILD_DIR/gcc.elf"
+printf '%s\n' 'PASS compiler/c0/core-integer-foundation-m26'
