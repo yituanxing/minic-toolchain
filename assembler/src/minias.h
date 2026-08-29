892375fefe12ee789323eb1bbae2d50d7711e43e
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-29 22:15:13 +0800

    assembler: expose selectable ELF class

--- assembler/src/minias.h
+++ assembler/src/minias.h
@@ -1,8 +1,13 @@
 #ifndef MINIAS_H
 #define MINIAS_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 int minias_assemble_file(const char *input_path, const char *output_path, FILE *diagnostic);
+int minias_assemble_file_class(const char *input_path,
+                               const char *output_path,
+                               bool elf32,
+                               FILE *diagnostic);
 
 #endif
