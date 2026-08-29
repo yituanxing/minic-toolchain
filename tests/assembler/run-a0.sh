280fc59f1178d7ad01f3e1dbdfeaeff9ec545109
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-29 22:16:15 +0800

    test: cover GCC size pseudos and ELF32 compat path [minias-linux-runtime]

--- tests/assembler/run-a0.sh
+++ tests/assembler/run-a0.sh
@@ -1057,4 +1057,48 @@ if readelf -Ws "$work/gcc-vdso-plt.o" | grep -q 'riscv_hwprobe@plt'; then
     exit 1
 fi
 
+cat >"$work/gcc-size-before-label.s" <<'EOF'
+.data
+.globl gcc_size_before_label
+.type gcc_size_before_label, @object
+.size gcc_size_before_label, 8
+gcc_size_before_label:
+  .dword 0x1122334455667788
+EOF
+"$MINIAS" -o "$work/gcc-size-before-label.o" "$work/gcc-size-before-label.s"
+test "$(readelf -Ws "$work/gcc-size-before-label.o" | awk '$8=="gcc_size_before_label" {print $3}')" = "8"
+
+cat >"$work/gcc-sgt-pseudos.s" <<'EOF'
+.text
+.globl gcc_sgt_pseudos
+.type gcc_sgt_pseudos, @function
+gcc_sgt_pseudos:
+  sgt a0, a1, a2
+  sgtu a3, a4, a5
+.size gcc_sgt_pseudos, . - gcc_sgt_pseudos
+EOF
+"$MINIAS" -o "$work/gcc-sgt-pseudos.o" "$work/gcc-sgt-pseudos.s"
+gcc_sgt_hex="$(
+    readelf -x .text "$work/gcc-sgt-pseudos.o" |
+    awk '/0x[0-9a-f]+/ {for (i=2; i<=NF; ++i) if ($i ~ /^[0-9a-f]{8}$/) printf "%s", $i}'
+)"
+test "$gcc_sgt_hex" = "3325b600b3b6e700"
+
+cat >"$work/rv32-elf.s" <<'EOF'
+.text
+.extern rv32_external
+.globl rv32_entry
+.type rv32_entry, @function
+rv32_entry:
+  call rv32_external
+  ret
+.size rv32_entry, . - rv32_entry
+EOF
+"$MINIAS" -march=rv32imac -mabi=ilp32 -o "$work/rv32-elf.o" "$work/rv32-elf.s"
+readelf -h "$work/rv32-elf.o" >"$work/rv32-elf.hdr"
+grep -q 'Class:[[:space:]]*ELF32' "$work/rv32-elf.hdr"
+grep -q 'Machine:[[:space:]]*RISC-V' "$work/rv32-elf.hdr"
+grep -q 'REL (Relocatable file)' "$work/rv32-elf.hdr"
+readelf -Wr "$work/rv32-elf.o" | grep -Eq 'R_RISCV_CALL_PLT[[:space:]]+.*rv32_external'
+
 echo "MINIAS_A0=PASS objects=27 format=ELF64-RISCV-ET_REL relocations=23 strings=2 pseudos=17 previous=3 subsection=2 numeric_labels=18 ecall=1 isa_next=13 csr_amo=8 sfence_vma=3 fence_i=1 vsetvl=1 vsetvli=1 vmv_v_i=4 rept=2 nested_rept=1 irp=4 section_stack=1 org=3 local_difference=1 lr_sc=2 inline_labels=2 branch_pseudos=8 extern=1 symbol_minus_dot=4 symbol_difference=1 absolute32=1 jal=2 jal_subsection=1 high_numeric_labels=2 u64_data=3 raw_insn=4 set_alias=3 move=1 numeric_zero=1 immediate_product=2 shift_immediate=1 incbin=1 align_expr=1 native_expr=6 native_zbb_tail_memexpr=1 native_jalr_symbol_load=1 native_forward_short=1 native_mixed_macro=1 native_divided_difference=1 native_symbol_addend_expr=1 native_add_sub_immediate=1 native_cfi_boundary=1 native_gas_forms=1 native_set_size=1 native_macro_comma=1 native_macro_quotes=1 sext_w=1 macro=4 conditional=1"
