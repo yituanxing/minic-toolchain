568406eddd7a501fc12706086c03ae8a710deb65
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-29 22:47:32 +0800

    test: lock far conditional branch relaxation [minias-linux-runtime]

--- tests/assembler/run-a0.sh
+++ tests/assembler/run-a0.sh
@@ -1122,4 +1122,23 @@ readelf -Ws "$work/gcc-lanchor-set.o" >"$work/gcc-lanchor-set.sym"
 grep -Eq '[[:space:]]\.LANCHOR0$' "$work/gcc-lanchor-set.sym"
 readelf -Wr "$work/gcc-lanchor-set.o" | grep -Eq 'R_RISCV_PCREL_HI20[[:space:]]+.*\.LANCHOR0'
 
+cat >"$work/far-conditional-branch.s" <<'EOF'
+.text
+.globl far_conditional_branch
+.type far_conditional_branch, @function
+far_conditional_branch:
+  beq a0, a1, .Lfar_target
+  .space 5000
+.Lfar_target:
+  ret
+.size far_conditional_branch, . - far_conditional_branch
+EOF
+"$MINIAS" -o "$work/far-conditional-branch.o" "$work/far-conditional-branch.s"
+far_branch_prefix="$(
+    readelf -x .text "$work/far-conditional-branch.o" |
+    awk '/0x00000000/ {print $2 $3; exit}'
+)"
+test "$far_branch_prefix" = "6314b5006f10c038"
+test "$(readelf -Ws "$work/far-conditional-branch.o" | awk '$8=="far_conditional_branch" {print $3}')" = "5012"
+
 echo "MINIAS_A0=PASS objects=27 format=ELF64-RISCV-ET_REL relocations=23 strings=2 pseudos=17 previous=3 subsection=2 numeric_labels=18 ecall=1 isa_next=13 csr_amo=8 sfence_vma=3 fence_i=1 vsetvl=1 vsetvli=1 vmv_v_i=4 rept=2 nested_rept=1 irp=4 section_stack=1 org=3 local_difference=1 lr_sc=2 inline_labels=2 branch_pseudos=8 extern=1 symbol_minus_dot=4 symbol_difference=1 absolute32=1 jal=2 jal_subsection=1 high_numeric_labels=2 u64_data=3 raw_insn=4 set_alias=3 move=1 numeric_zero=1 immediate_product=2 shift_immediate=1 incbin=1 align_expr=1 native_expr=6 native_zbb_tail_memexpr=1 native_jalr_symbol_load=1 native_forward_short=1 native_mixed_macro=1 native_divided_difference=1 native_symbol_addend_expr=1 native_add_sub_immediate=1 native_cfi_boundary=1 native_gas_forms=1 native_set_size=1 native_macro_comma=1 native_macro_quotes=1 sext_w=1 macro=4 conditional=1"
