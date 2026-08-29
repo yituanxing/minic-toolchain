f314a6fb2df7c2f110b5853654170edf7bc39749
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-29 17:42:22 +0800

    test: lock Linux Zbb tail and memory-expression forms [minias-native26]

--- tests/assembler/run-a0.sh
+++ tests/assembler/run-a0.sh
@@ -831,4 +831,27 @@ vector_hex="$(
 )"
 test "$vector_hex" = "d772300c57b00f5e57b40f5e57b80f5e57bc0f5e27000e0227040e0207d1050287040400"
 
-echo "MINIAS_A0=PASS objects=19 format=ELF64-RISCV-ET_REL relocations=16 strings=2 pseudos=17 previous=3 subsection=2 numeric_labels=18 ecall=1 isa_next=13 csr_amo=8 sfence_vma=3 fence_i=1 vsetvl=1 vsetvli=1 vmv_v_i=4 rept=2 nested_rept=1 irp=4 section_stack=1 org=3 local_difference=1 lr_sc=2 inline_labels=2 branch_pseudos=8 extern=1 symbol_minus_dot=4 symbol_difference=1 absolute32=1 jal=2 jal_subsection=1 high_numeric_labels=2 u64_data=3 raw_insn=4 set_alias=3 move=1 numeric_zero=1 immediate_product=2 shift_immediate=1 incbin=1 align_expr=1 native_expr=6 native_gas_forms=1 native_set_size=1 native_macro_comma=1 native_macro_quotes=1 sext_w=1 macro=4 conditional=1"
+cat >"$work/native-zbb-tail-memexpr.s" <<'EOF'
+.text
+.extern external_tail
+.globl native_zbb_tail_memexpr
+.type native_zbb_tail_memexpr, @function
+native_zbb_tail_memexpr:
+  orc.b a0, a1
+  rev8 a2, a3
+  ctz a4, a5
+  ld t0, (0 + 8)(a0)
+  sd t1, ((0 * 8) - (2 * 8))(t3)
+  tail external_tail
+.size native_zbb_tail_memexpr, . - native_zbb_tail_memexpr
+EOF
+"$MINIAS" -o "$work/native-zbb-tail-memexpr.o" "$work/native-zbb-tail-memexpr.s"
+native_zbb_tail_memexpr_hex="$(
+    readelf -x .text "$work/native-zbb-tail-memexpr.o" |
+    awk '/0x[0-9a-f]+/ {for (i=2; i<=NF; ++i) if ($i ~ /^[0-9a-f]{8}$/) printf "%s", $i}'
+)"
+test "$native_zbb_tail_memexpr_hex" = "13d5752813d6866b139717608332850023386efe1703000067000300"
+readelf -r "$work/native-zbb-tail-memexpr.o" | grep -Eq 'R_RISCV_CALL_PLT[[:space:]]+.*external_tail'
+test "$(readelf -Ws "$work/native-zbb-tail-memexpr.o" | awk '$8=="native_zbb_tail_memexpr" {print $3}')" = "28"
+
+echo "MINIAS_A0=PASS objects=20 format=ELF64-RISCV-ET_REL relocations=17 strings=2 pseudos=17 previous=3 subsection=2 numeric_labels=18 ecall=1 isa_next=13 csr_amo=8 sfence_vma=3 fence_i=1 vsetvl=1 vsetvli=1 vmv_v_i=4 rept=2 nested_rept=1 irp=4 section_stack=1 org=3 local_difference=1 lr_sc=2 inline_labels=2 branch_pseudos=8 extern=1 symbol_minus_dot=4 symbol_difference=1 absolute32=1 jal=2 jal_subsection=1 high_numeric_labels=2 u64_data=3 raw_insn=4 set_alias=3 move=1 numeric_zero=1 immediate_product=2 shift_immediate=1 incbin=1 align_expr=1 native_expr=6 native_zbb_tail_memexpr=1 native_gas_forms=1 native_set_size=1 native_macro_comma=1 native_macro_quotes=1 sext_w=1 macro=4 conditional=1"
