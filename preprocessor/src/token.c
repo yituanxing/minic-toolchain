5af52e652aae0638c62fb27a56cdcbb650a4180d
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-08-30 16:33:15 +0800

    pp: consume post-stringize provenance in _Pragma [minipp-a0] [minipp-linux-focus]

--- preprocessor/src/token.c
+++ preprocessor/src/token.c
@@ -396,7 +396,8 @@ static bool minipp_expand_pragma_operator(MiniPpState *state,
 
         ++cursor;
         while (text[cursor] == ' ' || text[cursor] == '\t' ||
-               text[cursor] == '\v' || text[cursor] == '\f') {
+               text[cursor] == '\v' || text[cursor] == '\f' ||
+               text[cursor] == '\a' || text[cursor] == '\b') {
             ++cursor;
         }
         if (text[cursor] != ')') {
