f8f5268981535572e79fa67b84251942c36d9fb3
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-01 16:11:20 +0800

    linker: keep objects inside GNU archive groups [minild-busybox-static]

--- tools/minic-ld/main.c
+++ tools/minic-ld/main.c
@@ -103,11 +103,17 @@ int main(int argc, char **argv) {
                 return 2;
             }
             inputs[input_count].path = argv[i];
-            if (has_archive_suffix(argv[i])) {
+            if (group_mode) {
+                /*
+                 * GNU --start-group applies to the complete input sequence,
+                 * including ordinary .o files. Objects appearing after an
+                 * archive may introduce new unresolved symbols that require
+                 * an earlier archive to be rescanned.
+                 */
+                inputs[input_count].kind = MINILD_INPUT_GROUP_ARCHIVE;
+            } else if (has_archive_suffix(argv[i])) {
                 if (whole_archive) {
                     inputs[input_count].kind = MINILD_INPUT_WHOLE_ARCHIVE;
-                } else if (group_mode) {
-                    inputs[input_count].kind = MINILD_INPUT_GROUP_ARCHIVE;
                 } else {
                     inputs[input_count].kind = MINILD_INPUT_ARCHIVE;
                 }
