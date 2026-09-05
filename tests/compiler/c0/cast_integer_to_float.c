3edfd81286b085d72b89ff2e653c344df33c24cc
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-05 16:52:20 +0800

    test: add binary32 cast and literal source

---
+++ tests/compiler/c0/cast_integer_to_float.c
@@ -0,0 +1,14 @@
+float cast_integer_to_float(int value)
+{
+    return (float)value;
+}
+
+double overflow_float_to_double(void)
+{
+    return (double)1e5000f;
+}
+
+float quarter_float(void)
+{
+    return .25f;
+}
