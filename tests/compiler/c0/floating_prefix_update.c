2f7390c2b739fa94bc7a3b8f854e6ec6c6e568e0
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-08 16:25:56 +0800

    tests: cover floating prefix updates

---
+++ tests/compiler/c0/floating_prefix_update.c
@@ -0,0 +1,17 @@
+double decrement_timeout(double timeout)
+{
+    --timeout;
+    return timeout;
+}
+
+float increment_ratio(float ratio)
+{
+    ++ratio;
+    return ratio;
+}
+
+long double increment_wide(long double value)
+{
+    ++value;
+    return value;
+}
