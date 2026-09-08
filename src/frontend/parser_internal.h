2deb42f47920bb7980948f0b6bf565eda9453890
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-08 13:26:49 +0800

    frontend: expose variadic argument conversion

--- src/frontend/parser_internal.h
+++ src/frontend/parser_internal.h
@@ -453,6 +453,8 @@ bool minic_parser_parse_direct_member(MinicParser *parser,
 bool minic_parser_apply_fixed_call_argument_conversion(MinicParser *parser,
                                                        MinicType target_type,
                                                        MinicExpressionId *argument_id);
+bool minic_parser_apply_variadic_argument_conversion(MinicParser *parser,
+                                                          MinicExpressionId *argument_id);
 bool minic_parser_apply_array_decay(MinicParser *parser,
                                     MinicExpressionId input_id,
                                     MinicExpressionId *expression_id);
