f8ad2654f0d6ede507d52e9da5bd318dab2703f4
LiYang Dong <119646676+yituanxing@users.noreply.github.com>
2026-09-15 23:02:29 +0800

    test: add Linux-style csr_swap declaration initializer probe

---
+++ tests/compiler/c0/gnu_inline_asm_satp_decl_init.c
@@ -0,0 +1,25 @@
+#define satp_swap_like(value)                                                \
+({                                                                           \
+    unsigned long __v = (unsigned long)(value);                              \
+    __asm__ __volatile__("csrrw %0, satp, %1"                                \
+                         : "=r"(__v)                                         \
+                         : "rK"(__v)                                         \
+                         : "memory");                                        \
+    __v;                                                                     \
+})
+
+static unsigned long satp_decl_transition(unsigned long identity_satp) {
+    unsigned long old_satp;
+
+    __asm__ __volatile__("csrw satp, %0"
+                         :
+                         : "rK"(identity_satp)
+                         : "memory");
+
+    old_satp = satp_swap_like(0UL);
+    return old_satp;
+}
+
+unsigned long satp_decl_window_probe(unsigned long identity_satp) {
+    return satp_decl_transition(identity_satp);
+}
